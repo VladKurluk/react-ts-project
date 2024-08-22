c8376104dd31ddab078ce8bb74b1850df1cf6252
Vlad Kurluk <vlad11kurluk89@gmail.com>
2024-08-22 16:14:02 +0300

    Created Github UserCard component for displaying user info

---
+++ src/mock/index.ts
@@ -0,0 +1,18 @@
+import { LocalGithubUser } from "../types/user";
+
+export const defaultUser: LocalGithubUser = {
+    "login": "VladKurluk",
+    "id": 21339095,
+    "avatar": "https://avatars.githubusercontent.com/u/21339095?v=4",
+    "name": "Vladislav Kurluk",
+    "company": "",
+    "blog": "",
+    "location": "Ukraine",
+    "bio": "I'm involved in software development. I use JavaScript in my work.",
+    "twitter": "",
+    "repos": 14,
+    "followers": 2,
+    "following": 13,
+    "created": "2016-08-30T14:06:44Z",
+}
+  
